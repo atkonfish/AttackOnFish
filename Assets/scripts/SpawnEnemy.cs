b00645e761b97aebe12bd06552eea2c384ae247f
kevin791129 <kevin791129@gmail.com>
2016-02-04 13:11:47 -0500

    Combined Numair's Work
    2 files could not merge properly
    Manually went through, fixed some codes, changed some colliders from box
    to polygon

--- Assets/scripts/SpawnEnemy.cs
+++ Assets/scripts/SpawnEnemy.cs
@@ -3,14 +3,54 @@ using System.Collections;
 
 public class SpawnEnemy : MonoBehaviour {
    
-	[SerializeField] private GameObject enemyPrefab;
+	[SerializeField] public GameObject enemyPrefab;
 	private GameObject _enemy;
-	
-	void Update () {
-		if (_enemy == null) {
-			_enemy = Instantiate(enemyPrefab) as GameObject;
-			float spawnY = Random.Range(-4.2f, 4.2f);
-			_enemy.transform.position = new Vector3(10, spawnY, 0);;
-		}
-	}
+    public float spawnTime = 3f;
+    GameObject Player;
+    int Playerhp;
+    PlayerHealth PlayerHealthScript;
+    bool spawningCheck = true;
+
+
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("player");
+        PlayerHealthScript = Player.GetComponent<PlayerHealth>();
+
+
+        if (spawningCheck)
+        {
+            InvokeRepeating("Spawn", spawnTime, spawnTime);
+        }
+
+        
+
+       // if (PlayerHealthScript.hp > 0)
+        //{
+           // InvokeRepeating("Spawn", spawnTime, spawnTime);
+        //}
+    }
+
+    void Update()
+
+    {
+        if (spawningCheck == false)
+        {
+            CancelInvoke("Spawn");
+        }
+
+        // Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
+
+        if (PlayerHealthScript.hp <= 0)
+        {
+            spawningCheck = false;
+        }
+    }
+
+    void Spawn ()
+    {
+		float spawnY = Random.Range(-4.2f, 4.2f);
+        _enemy = (GameObject)Instantiate(enemyPrefab, new Vector3(10, spawnY, 0), Quaternion.identity);
+    }
+
 }
--- Assets/scripts/itemSpawn.cs
+++ Assets/scripts/itemSpawn.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class itemSpawn : MonoBehaviour {
 
-	[SerializeField] private GameObject enemyPrefab;
+	[SerializeField] private GameObject itemPrefab;
 	private GameObject _item;
 	public float itemBaseInterval = 10.0f;
 	int itemChance;
@@ -17,7 +17,7 @@ public class itemSpawn : MonoBehaviour {
 	void Update () {
 		itemInterval -= Time.deltaTime;
 		if (itemInterval <= 0) {
-			_item = Instantiate(enemyPrefab) as GameObject;
+			_item = Instantiate(itemPrefab) as GameObject;
 			float spawnY = Random.Range(-4.2f, 4.2f);
 			_item.transform.position = new Vector3(10, spawnY, 0);
 			itemChance = Random.Range(1, 4);
